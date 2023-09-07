629b6d8d218dbf9fa73716110112850edabddb9b
PiotrZak <piotrzak77@gmail.com>
2023-09-07 16:36:09 +0200

    fix

--- Synthapse.Frontend/src/App.test.tsx
+++ Synthapse.Frontend/src/App.test.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import App, { Hero } from './App';
 import { shallow, configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+configure({adapter: new Adapter()});
 
 test('renders learn react link', () => {
   render(<App />);
